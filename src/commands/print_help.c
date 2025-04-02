255327e0f82a9d72660ee2070e65531c94206809
MiracleCanCode <pashadurov12@gmail.com>
2025-04-02 09:23:22 +0300

    update: help arg

--- src/commands/print_help.c
+++ src/commands/print_help.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "commands.h"
 
-#define COPY_FILE_DATA_TEXT " -from [ФАЙЛ] -to [ФАЙЛ] скопировать откуда - куда\n"
-#define READ_COMMAND_TEXT " -r [ФАЙЛ] чтение файла и вывод на стандартный вывод\n"
-#define REMARK_TEXT  "Использование: ./bin/isacat [ПАРАМЕТР] [ФАЙЛ]\n"\
-"Если комманда не задана, то читает стандартный ввод\n\n"
-#define SEARCH_COMMAND_TEXT " -s [ФАЙЛ] -w [СЛОВО] найти слово в файле\n"
+#define COPY_FILE_DATA_TEXT " -from [ФАЙЛ] -to [ФАЙЛ] скопировать откуда - куда"
+#define READ_COMMAND_TEXT " -r [ФАЙЛ] чтение файла и вывод на стандартный вывод"
+#define REMARK_TEXT  "Использование: ./bin/isacat [ПАРАМЕТР] [ФАЙЛ] "\
+"Если комманда не задана, то читает стандартный ввод\n"
+#define SEARCH_COMMAND_TEXT " -s [ФАЙЛ] -w [СЛОВО] найти слово в файле"
 #define PRINT_INFO_FILE_TEXT " -info [ФАЙЛ] вывести информацию о файле"
 
 void print_help(void)
 {
-   printf(REMARK_TEXT); 
-   printf(COPY_FILE_DATA_TEXT);
-   printf(READ_COMMAND_TEXT);
-   printf(SEARCH_COMMAND_TEXT);
+   puts(REMARK_TEXT); 
+   puts(COPY_FILE_DATA_TEXT);
+   puts(READ_COMMAND_TEXT);
+   puts(SEARCH_COMMAND_TEXT);
    puts(PRINT_INFO_FILE_TEXT);
 }
